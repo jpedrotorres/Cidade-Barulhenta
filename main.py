4b1d7f2c830ff09804f681e4e3d1f9bb8e2267f1
billy <joaodev.pedro@gmail.com>
2025-05-30 18:59:45 -0300

    Adicionando função para encerrar programa

--- main.py
+++ main.py
@@ -6,37 +6,44 @@ import numpy as np
 
 class IntensidadeSom:
 	def __init__(self):
-		self.samplerate = 44100
-		self.blocksize = int(self.samplerate * 0.5)
-		self.channels = 1
-		self.EPSILON = 1e-10
+		self.samplerate=44100
+		self.blocksize=int(self.samplerate * 0.5)
+		self.channels=1
+		self.EPSILON=1e-10
 
 	def calc_dbfs(self, indata, frames, time, status):
-		self.rms= np.sqrt(np.mean(indata**2) + self.EPSILON)
-		self.dbfs= 20 * np.log10(self.rms)
+		self.rms=np.sqrt(np.mean(indata**2) + self.EPSILON)
+		self.dbfs=20 * np.log10(self.rms)
 
 class TelaProjeto:
 	def __init__(self, root):
-		self.root= root
+		self.root=root
 		root.title("Som e Sustentabilidade")
 #		root.geometry("300x500")
 
-		self.som= IntensidadeSom()
+		self.som=IntensidadeSom()
 		self.create_widgets()
 
 	def create_widgets(self):
-		self.title= tk.Label(self.root, text= "Absorção do Som por Difentes Materiais").pack()
+	#Criando menu
+		self.menu=tk.Menu(self.root)
 
-		self.menu= tk.Menu(self.root)
-		self.project_menu= tk.Menu(self.menu, tearoff= 0)
-		self.menu.add_cascade(label="Projeto", menu= self.project_menu)
+		self.project_menu=tk.Menu(self.menu, tearoff=0)
+		self.menu.add_cascade(label="Projeto", menu=self.project_menu)
 		self.project_menu.add_command(label="Novo")
-		self.project_menu.add_command(label="Sair")
+		self.project_menu.add_separator()
+		self.project_menu.add_command(label="Sair", command= self.root.quit)
 		self.root.config(menu=self.menu)
 
-if __name__=="__main__":
-	root= tk.Tk()
+		self.config_menu=tk.Menu(self.menu, tearoff=0)
 
-	app= TelaProjeto(root)
+	#Criando elementos do corpo
+		self.title=tk.Label(self.root, text="Absorção do Som por Difentes Materiais").pack()
+
+
+if __name__=="__main__":
+	root=tk.Tk()
+
+	app=TelaProjeto(root)
 
 	root.mainloop()
